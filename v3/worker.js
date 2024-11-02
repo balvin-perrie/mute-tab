0936ed678b7443d9116231107b3c101950176b22
Balvin Perrie <balvin.perrie@gmail.com>
2024-11-02 08:53:50 -0400

    version 0.2.3

--- v3/worker.js
+++ v3/worker.js
@@ -241,14 +241,18 @@ chrome.storage.onChanged.addListener(ps => {
   }
 });
 {
-  const once = () => chrome.storage.local.get({
-    'page-context': false
-  }, prefs => contexts(prefs['page-context']));
+  const once = () => {
+    if (once.done) {
+      return;
+    }
+    once.done = true;
+    chrome.storage.local.get({
+      'page-context': false
+    }, prefs => contexts(prefs['page-context']));
+  };
 
   chrome.runtime.onInstalled.addListener(once);
-  if (/Firefox/.test(navigator.userAgent)) {
-    chrome.runtime.onStartup.addListener(once);
-  }
+  chrome.runtime.onStartup.addListener(once);
 }
 
 
@@ -256,8 +260,7 @@ chrome.storage.onChanged.addListener(ps => {
 {
   const {management, runtime: {onInstalled, setUninstallURL, getManifest}, storage, tabs} = chrome;
   if (navigator.webdriver !== true) {
-    const page = getManifest().homepage_url;
-    const {name, version} = getManifest();
+    const {homepage_url: page, name, version} = getManifest();
     onInstalled.addListener(({reason, previousVersion}) => {
       management.getSelf(({installType}) => installType === 'normal' && storage.local.get({
         'faqs': true,
@@ -266,7 +269,7 @@ chrome.storage.onChanged.addListener(ps => {
         if (reason === 'install' || (prefs.faqs && reason === 'update')) {
           const doUpdate = (Date.now() - prefs['last-update']) / 1000 / 60 / 60 / 24 > 45;
           if (doUpdate && previousVersion !== version) {
-            tabs.query({active: true, currentWindow: true}, tbs => tabs.create({
+            tabs.query({active: true, lastFocusedWindow: true}, tbs => tabs.create({
               url: page + '?version=' + version + (previousVersion ? '&p=' + previousVersion : '') + '&type=' + reason,
               active: reason === 'install',
               ...(tbs && tbs.length && {index: tbs[0].index + 1})
